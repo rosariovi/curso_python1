1bdcf1351a162ce04aa67026e0815a7a0ca207e5
rosariovi <soloaqui002@gamil.com>
2019-08-30 18:13:43 -0400

    ciclo for

---
+++ ciclo_for.py
@@ -0,0 +1,4 @@
+print("TABLA DEL 5")
+for i in range (1,11):
+    a=i*5
+    print("5 * ", i, " = ", a)
--- mayor2.py
+++ mayor2.py
@@ -7,6 +7,6 @@ c=int(input())
 if a>b and a>c:
     print("el numero mayor es  ", a)
 elif b>a and b>c:
-    print("el numero mayor es  ", b)
+    print("el numero mayor es el segundo numero =  ", b)
 else:
-    print("el numero mayor es  ", c)
+    print("el numero mayor es el tercer numero =  ", c)
